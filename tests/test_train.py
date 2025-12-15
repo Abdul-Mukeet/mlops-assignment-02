0173e6ee520101a2b8888802d633e0961b6ce743
your_username <mukeet0123@gmail.com>
2025-12-15 19:52:45 +0500

    Update test to use sample_dataset.csv

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,10 +1,9 @@
 import os
 import pandas as pd
 
-
 def test_data_loading():
     base_dir = os.path.dirname(__file__)
-    # Use sample dataset for CI
-    data_path = os.path.join(base_dir, "..", "data", "dataset.csv")
+    # Update file name here
+    data_path = os.path.join(base_dir, "..", "data", "sample_dataset.csv")
     data = pd.read_csv(data_path)
     assert not data.empty
