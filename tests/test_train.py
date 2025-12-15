1e9c9e32952ef19254a6f66cd598fb434c1af42c
your_username <mukeet0123@gmail.com>
2025-12-15 19:37:43 +0500

    Update test_train.py to use sample dataset for CI

--- tests/test_train.py
+++ tests/test_train.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 
 def test_data_loading():
-    # Make path relative to test file
     base_dir = os.path.dirname(__file__)
-    data_path = os.path.join(base_dir, "..", "data", "dataset.csv")
-
+    # Use sample dataset for CI
+    data_path = os.path.join(base_dir, "..", "data", "sample_dataset.csv")
     data = pd.read_csv(data_path)
     assert not data.empty
